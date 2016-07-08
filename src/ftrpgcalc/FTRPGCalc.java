ca7a36bde86a182ce4012bd7b54339d4c221dd41
Alunoinf_2 <Alunoinf_2@PC-606477>
2016-07-08 18:24:09 -0300

    Commit Inicial da Classe FTRPGCalc

---
+++ src/ftrpgcalc/FTRPGCalc.java
@@ -0,0 +1,15 @@
+package ftrpgcalc;
+
+/**
+ * Classe principal que inicializa a aplicação.
+ * @author Doigyfu
+ */
+public class FTRPGCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
