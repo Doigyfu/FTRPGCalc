d7ed49927e9428e0230d83deb79f1bc545541ddf
Alunoinf_2 <Alunoinf_2@PC-606477>
2016-07-08 18:32:09 -0300

    Commit Inicial da Classe Calculadora.

---
+++ src/ftrpgcalc/Calculadora.java
@@ -0,0 +1,10 @@
+package ftrpgcalc;
+
+/**
+ * Classe que armazena os dados de atributos e efetua suas operações.
+ * @author Alunoinf_2
+ */
+public class Calculadora {
+    int força, agilidade, vitalidade, destreza, inteligencia, percepcao, carisma,
+            controleMagico, resistencia;
+}
